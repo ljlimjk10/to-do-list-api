833d899795edebadb38fd0e6144729a4105d7998
Jun Kai <ljlimjk10@gmail.com>
2021-06-05 17:16:17 +0800

    Adding .env

--- todolist/todolist/settings.py
+++ todolist/todolist/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 """
 
 from pathlib import Path
+from decouple import config
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve(strict=True).parent.parent
@@ -20,7 +21,7 @@ BASE_DIR = Path(__file__).resolve(strict=True).parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 't_-%q-d99j(6%@(h53m!u!3+g%#rvxm-m@$7$$io*et8)098rj'
+SECRET_KEY = config("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
